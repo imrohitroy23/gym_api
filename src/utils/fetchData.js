a46395d483653cf3bd79ba7fd4ec2196fa98a00c
ROHIT ROY <90301368+imrohitroy23@users.noreply.github.com>
2023-05-18 16:16:24 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '3deab57297mshb310158e16869a0p1d87d7jsn04a1d2c0928b',
+    'X-RapidAPI-Key': '3deab57297mshb310158e16869a0p1d87d7jsn04a1d2c0928',
   'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
